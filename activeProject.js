da933a019bdbb01f1afc9a465ac9a548d42d8640
chchew1110 <chchew1110@gmail.com>
2018-07-09 19:42:07 -0700

    Added BestlineAward

--- activeProject.js
+++ activeProject.js
@@ -11,8 +11,8 @@ function selectProject(id)
         case "tictactoe":
            $("#previewpanel").load("../MyWebsite/TicTacToe/tictactoe.html");
         break;
-        case "musicnotes":
-            document.getElementById("previewpanel").innerHTML = "<p>Insert Music Note HTML page here</p>";
+        case "bestline":
+            $("#previewpanel").load("../MyWebsite/bestline.html");
         break;
     }
 }
