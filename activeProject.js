f4791e5d519018cec707772d5a3cd9aceb3d3361
chchew1110 <chchew1110@gmail.com>
2018-07-12 18:16:54 -0700

    Modify the active project javascript

--- activeProject.js
+++ activeProject.js
@@ -4,15 +4,19 @@ function selectProject(id)
 {
     if(active !== null)
     {
-          document.getElementById("previewpanel").innerHTML = "";     
+        $(active).css('background-color', ''); 
     }
     switch(id)
     {
         case "tictactoe":
-           $("#previewpanel").load("../MyWebsite/TicTacToe/tictactoe.html");
+            $("#previewpanel").load("../MyWebsite/TicTacToe/tictactoe.html");
+            $('#tictactoe').css('background-color', 'black');
+            active = '#'+id;
         break;
         case "bestline":
             $("#previewpanel").load("../MyWebsite/bestline.html");
+            $('#bestline').css('background-color', 'black');
+            active = '#'+id;
         break;
     }
 }
